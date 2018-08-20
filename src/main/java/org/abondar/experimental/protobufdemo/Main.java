5a58c4ca77486e326a1920d42375d80487bfb0ba
abondar24 <abondar1992@gmail.com>
2018-08-20 08:09:54 +0200

    Maven setup and logging

--- src/main/java/org/abondar/experimental/protobufdemo/Main.java
+++ src/main/java/org/abondar/experimental/protobufdemo/Main.java
@@ -2,18 +2,22 @@ package org.abondar.experimental.protobufdemo;
 
 import org.abondar.experimental.protobufdemo.client.ClientThread;
 import org.abondar.experimental.protobufdemo.server.ServerThread;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.util.logging.Logger;
 
 public class Main {
 
-    //private static Logger logger = Logger.getLogger();
+    private static Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
 
+        logger.info("Starting demo");
         ServerThread server = new ServerThread();
         server.run();
 
         ClientThread client = new ClientThread();
         client.run();
+
+
     }
 }
